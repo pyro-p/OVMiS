5d071a4cebd776ca0622b04ce83d90483f078b04
Pyro <732603@gmail.com>
2024-11-22 03:22:34 +0400

    Stage commit

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -28,12 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            comboBoxOutNS = new ComboBox();
+            comboBoxInNS = new ComboBox();
+            textBoxInput = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBoxOutput = new TextBox();
+            buttonCalculate = new Button();
+            SuspendLayout();
+            // 
+            // comboBoxOutNS
+            // 
+            comboBoxOutNS.FormattingEnabled = true;
+            comboBoxOutNS.Items.AddRange(new object[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16" });
+            comboBoxOutNS.Location = new Point(97, 41);
+            comboBoxOutNS.Name = "comboBoxOutNS";
+            comboBoxOutNS.Size = new Size(109, 23);
+            comboBoxOutNS.TabIndex = 0;
+            comboBoxOutNS.SelectedIndexChanged += ComboBoxOutNS_SelectedIndexChanged;
+            // 
+            // comboBoxInNS
+            // 
+            comboBoxInNS.FormattingEnabled = true;
+            comboBoxInNS.Items.AddRange(new object[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16" });
+            comboBoxInNS.Location = new Point(97, 12);
+            comboBoxInNS.Name = "comboBoxInNS";
+            comboBoxInNS.Size = new Size(109, 23);
+            comboBoxInNS.TabIndex = 1;
+            comboBoxInNS.SelectedIndexChanged += ComboBoxInNS_SelectedIndexChanged;
+            // 
+            // textBoxInput
+            // 
+            textBoxInput.Location = new Point(97, 70);
+            textBoxInput.Name = "textBoxInput";
+            textBoxInput.Size = new Size(183, 23);
+            textBoxInput.TabIndex = 2;
+            textBoxInput.TextChanged += TextBoxInput_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(79, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Исходная СС";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(79, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Конечная СС";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 73);
+            label3.Name = "label3";
+            label3.Size = new Size(33, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Ввод";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 102);
+            label4.Name = "label4";
+            label4.Size = new Size(42, 15);
+            label4.TabIndex = 6;
+            label4.Text = "Вывод";
+            // 
+            // textBoxOutput
+            // 
+            textBoxOutput.Location = new Point(97, 99);
+            textBoxOutput.Name = "textBoxOutput";
+            textBoxOutput.Size = new Size(183, 23);
+            textBoxOutput.TabIndex = 7;
+            textBoxOutput.TextChanged += TextBoxOutput_TextChanged;
+            // 
+            // buttonCalculate
+            // 
+            buttonCalculate.Location = new Point(97, 128);
+            buttonCalculate.Name = "buttonCalculate";
+            buttonCalculate.Size = new Size(75, 23);
+            buttonCalculate.TabIndex = 8;
+            buttonCalculate.Text = "Перевести";
+            buttonCalculate.UseVisualStyleBackColor = true;
+            buttonCalculate.Click += ButtonCalculate_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            ClientSize = new Size(369, 195);
+            Controls.Add(buttonCalculate);
+            Controls.Add(textBoxOutput);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBoxInput);
+            Controls.Add(comboBoxInNS);
+            Controls.Add(comboBoxOutNS);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ComboBox comboBoxOutNS;
+        private ComboBox comboBoxInNS;
+        private TextBox textBoxInput;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBoxOutput;
+        private Button buttonCalculate;
     }
 }
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -6,5 +6,50 @@ namespace lab3
         {
             InitializeComponent();
         }
+
+        private void ComboBoxInNS_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComboBoxOutNS_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxOutput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonCalculate_Click(object sender, EventArgs e)
+        {
+            int inNs = Convert.ToInt32(comboBoxInNS.SelectedText);
+            int outNS = Convert.ToInt32(comboBoxOutNS.SelectedText);
+
+            int input = Convert.ToInt32(textBoxInput.Text);
+            ConvertToDecimal(input, inNs);
+        }
+
+        private int ConvertToDecimal(int number, int numeralSystem)
+        {
+            string numString = number.ToString();
+            int[] numArray = new int[numString.Length];
+
+            for (int i = 0; i < numString.Length; i++)
+            {
+                numArray[i] = (int)Char.GetNumericValue(number.ToString()[i]);
+            }
+
+            int output = Convert.ToInt32(numArray);
+            return output;
+        }
+
+
     }
 }
