047488ca7b060100a006e865e1a4a62fdb7b00a7
Pyro <732603@gmail.com>
2024-12-10 05:19:18 +0400

    работает без отрицательных чисел

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -28,28 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            comboBoxOutNS = new ComboBox();
             comboBoxInNS = new ComboBox();
-            textBoxInput = new TextBox();
+            comboBoxOperation = new ComboBox();
+            textBoxInput1 = new TextBox();
+            textBoxInput2 = new TextBox();
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
             label4 = new Label();
-            textBoxOutput = new TextBox();
             buttonCalculate = new Button();
+            textBoxOutput = new TextBox();
             textBoxOutputDC = new TextBox();
             label5 = new Label();
+            label6 = new Label();
             SuspendLayout();
-            // 
-            // comboBoxOutNS
-            // 
-            comboBoxOutNS.FormattingEnabled = true;
-            comboBoxOutNS.Items.AddRange(new object[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16" });
-            comboBoxOutNS.Location = new Point(97, 41);
-            comboBoxOutNS.Name = "comboBoxOutNS";
-            comboBoxOutNS.Size = new Size(109, 23);
-            comboBoxOutNS.TabIndex = 0;
-            comboBoxOutNS.Text = "2";
             // 
             // comboBoxInNS
             // 
@@ -61,13 +53,31 @@
             comboBoxInNS.TabIndex = 1;
             comboBoxInNS.Text = "10";
             // 
-            // textBoxInput
+            // comboBoxOperation
             // 
-            textBoxInput.Location = new Point(97, 70);
-            textBoxInput.MaxLength = 11;
-            textBoxInput.Name = "textBoxInput";
-            textBoxInput.Size = new Size(261, 23);
-            textBoxInput.TabIndex = 2;
+            comboBoxOperation.FormattingEnabled = true;
+            comboBoxOperation.Items.AddRange(new object[] { "+", "-", "*", "/" });
+            comboBoxOperation.Location = new Point(97, 41);
+            comboBoxOperation.Name = "comboBoxOperation";
+            comboBoxOperation.Size = new Size(109, 23);
+            comboBoxOperation.TabIndex = 0;
+            comboBoxOperation.Text = "+";
+            // 
+            // textBoxInput1
+            // 
+            textBoxInput1.Location = new Point(97, 70);
+            textBoxInput1.MaxLength = 11;
+            textBoxInput1.Name = "textBoxInput1";
+            textBoxInput1.Size = new Size(261, 23);
+            textBoxInput1.TabIndex = 2;
+            // 
+            // textBoxInput2
+            // 
+            textBoxInput2.Location = new Point(97, 99);
+            textBoxInput2.MaxLength = 11;
+            textBoxInput2.Name = "textBoxInput2";
+            textBoxInput2.Size = new Size(261, 23);
+            textBoxInput2.TabIndex = 11;
             // 
             // label1
             // 
@@ -83,38 +93,31 @@
             label2.AutoSize = true;
             label2.Location = new Point(12, 44);
             label2.Name = "label2";
-            label2.Size = new Size(79, 15);
+            label2.Size = new Size(62, 15);
             label2.TabIndex = 4;
-            label2.Text = "Конечная СС";
+            label2.Text = "Операция";
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Location = new Point(12, 73);
             label3.Name = "label3";
-            label3.Size = new Size(33, 15);
+            label3.Size = new Size(50, 15);
             label3.TabIndex = 5;
-            label3.Text = "Ввод";
+            label3.Text = "1 число";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(12, 102);
+            label4.Location = new Point(12, 131);
             label4.Name = "label4";
             label4.Size = new Size(42, 15);
             label4.TabIndex = 6;
             label4.Text = "Вывод";
             // 
-            // textBoxOutput
-            // 
-            textBoxOutput.Location = new Point(97, 99);
-            textBoxOutput.Name = "textBoxOutput";
-            textBoxOutput.Size = new Size(261, 23);
-            textBoxOutput.TabIndex = 7;
-            // 
             // buttonCalculate
             // 
-            buttonCalculate.Location = new Point(97, 157);
+            buttonCalculate.Location = new Point(97, 186);
             buttonCalculate.Name = "buttonCalculate";
             buttonCalculate.Size = new Size(75, 23);
             buttonCalculate.TabIndex = 8;
@@ -122,9 +125,16 @@
             buttonCalculate.UseVisualStyleBackColor = true;
             buttonCalculate.Click += ButtonCalculate_Click;
             // 
+            // textBoxOutput
+            // 
+            textBoxOutput.Location = new Point(97, 128);
+            textBoxOutput.Name = "textBoxOutput";
+            textBoxOutput.Size = new Size(261, 23);
+            textBoxOutput.TabIndex = 7;
+            // 
             // textBoxOutputDC
             // 
-            textBoxOutputDC.Location = new Point(97, 128);
+            textBoxOutputDC.Location = new Point(97, 157);
             textBoxOutputDC.Name = "textBoxOutputDC";
             textBoxOutputDC.Size = new Size(261, 23);
             textBoxOutputDC.TabIndex = 9;
@@ -132,11 +142,20 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(12, 131);
+            label5.Location = new Point(12, 160);
             label5.Name = "label5";
             label5.Size = new Size(23, 15);
             label5.TabIndex = 10;
             label5.Text = "ПК";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 102);
+            label6.Name = "label6";
+            label6.Size = new Size(50, 15);
+            label6.TabIndex = 12;
+            label6.Text = "2 число";
             // 
             // Form1
             // 
@@ -144,7 +163,9 @@
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
             AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            ClientSize = new Size(385, 201);
+            ClientSize = new Size(385, 219);
+            Controls.Add(label6);
+            Controls.Add(textBoxInput2);
             Controls.Add(label5);
             Controls.Add(textBoxOutputDC);
             Controls.Add(buttonCalculate);
@@ -153,9 +174,9 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(textBoxInput);
+            Controls.Add(textBoxInput1);
             Controls.Add(comboBoxInNS);
-            Controls.Add(comboBoxOutNS);
+            Controls.Add(comboBoxOperation);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -164,9 +185,10 @@
 
         #endregion
 
-        private ComboBox comboBoxOutNS;
         private ComboBox comboBoxInNS;
-        private TextBox textBoxInput;
+        private ComboBox comboBoxOperation;
+        private TextBox textBoxInput1;
+        private TextBox textBoxInput2;
         private Label label1;
         private Label label2;
         private Label label3;
@@ -175,5 +197,6 @@
         private Button buttonCalculate;
         private TextBox textBoxOutputDC;
         private Label label5;
+        private Label label6;
     }
 }
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -12,35 +12,63 @@ namespace lab3
             try
             {
                 int inNS = int.Parse(comboBoxInNS.Text);
-                int outNS = int.Parse(comboBoxOutNS.Text);
-                string input = textBoxInput.Text;
+                string input1 = textBoxInput1.Text;
+                string input2 = textBoxInput2.Text;
+                string operation = comboBoxOperation.SelectedItem.ToString();
 
-                if (inNS < 2 || inNS > 16 || outNS < 2 || outNS > 16)
+
+                if (inNS < 2 || inNS > 16)
                 {
                     MessageBox.Show("Неверно указана система счисления");
                     return;
                 }
 
-                if (!CheckValidNum(input, inNS))
+                if (!CheckValidNum(input1, inNS) || !CheckValidNum(input2, inNS))
                 {
                     MessageBox.Show("Некорректное число");
                     return;
                 }
 
-                int decimalInput = ConvertToDecimal(input, inNS);
-                string convertedInput = ConvertFromDecimal(decimalInput, outNS);
+                int decimalInput1 = ConvertToDecimal(input1, inNS);
+                int decimalInput2 = ConvertToDecimal(input2, inNS);
 
-                if (outNS == 2)
+                int bitLength = GetRequiredBitLength(Math.Max(Math.Abs(decimalInput1), Math.Abs(decimalInput2)), inNS); // Определяем необходимую разрядность
+
+                string binary1 = ConvertFromDecimal(decimalInput1, 2).PadLeft(bitLength, (decimalInput1 < 0) ? '1' : '0');  // Дополняем до нужной разрядности
+                string binary2 = ConvertFromDecimal(decimalInput2, 2).PadLeft(bitLength, (decimalInput2 < 0) ? '1' : '0');
+
+                string resultBinary = "";
+
+                switch (operation)
                 {
-                    textBoxOutput.Text = convertedInput;
-                    textBoxOutputDC.Text = FormatBinary(ToDirectCode(decimalInput));
-                }
-                else
-                {
-                    textBoxOutput.Text = convertedInput;
+                    case "+":
+                        resultBinary = AddBinary(binary1, binary2);
+                        break;
+                    case "-":
+                        resultBinary = SubtractBinary(binary1, binary2);
+                        break;
+                    case "*":
+                        resultBinary = MultiplyBinary(binary1, binary2);
+                        break;
+                    case "/":
+                        if (decimalInput2 == 0)
+                        {
+                            MessageBox.Show("Деление на ноль!");
+                            return;
+                        }
+                        resultBinary = DivideBinary(binary1, binary2);
+                        break;
+                    default:
+                        MessageBox.Show("Неверная операция");
+                        return;
                 }
 
 
+                int resultDecimal = ConvertToDecimal(resultBinary, 2);
+                string resultInNS = ConvertFromDecimal(resultDecimal, inNS);
+
+                textBoxOutput.Text = resultInNS;
+                textBoxOutputDC.Text = FormatBinary(ToDirectCode(resultDecimal));
             }
             catch (FormatException)
             {
@@ -54,6 +82,204 @@ namespace lab3
             {
                 MessageBox.Show(ex.Message);
             }
+        }
+
+        private string AddBinary(string bin1, string bin2)
+        {
+            int maxLength = Math.Max(bin1.Length, bin2.Length);
+            bin1 = bin1.PadLeft(maxLength, '0');
+            bin2 = bin2.PadLeft(maxLength, '0');
+
+            char carry = '0';
+            string result = "";
+
+            for (int i = maxLength - 1; i >= 0; i--)
+            {
+                int sum = (bin1[i] - '0') + (bin2[i] - '0') + (carry - '0');
+                switch (sum)
+                {
+                    case 0:
+                        result = '0' + result;
+                        carry = '0';
+                        break;
+                    case 1:
+                        result = '1' + result;
+                        carry = '0';
+                        break;
+                    case 2:
+                        result = '0' + result;
+                        carry = '1';
+                        break;
+                    case 3:
+                        result = '1' + result;
+                        carry = '1';
+                        break;
+                }
+            }
+
+            if (carry == '1')
+            {
+                result = '1' + result;
+            }
+
+            return result;
+        }
+
+        private string SubtractBinary(string bin1, string bin2)
+        {
+            int maxLength = Math.Max(bin1.Length, bin2.Length);
+            bin1 = bin1.PadLeft(maxLength, '0');
+            bin2 = bin2.PadLeft(maxLength, '0');
+
+            string bin2Complement = TwosComplement(bin2);
+
+            string result = AddBinary(bin1, bin2Complement);
+
+            if (result.Length > maxLength)
+            {
+                result = result.Substring(1);
+            }
+
+            return result;
+        }
+
+        private string TwosComplement(string binary)
+        {
+            string inverted = "";
+            foreach (char bit in binary)
+            {
+                inverted += (bit == '0') ? '1' : '0';
+            }
+
+            return AddBinary(inverted, "1");
+        }
+
+        private string MultiplyBinary(string bin1, string bin2)
+        {
+            bool negativeResult = (bin1[0] == '1') ^ (bin2[0] == '1');
+
+            if (bin1.StartsWith("1")) bin1 = TwosComplement(bin1);
+            if (bin2.StartsWith("1")) bin2 = TwosComplement(bin2);
+
+            bin1 = bin1.TrimStart('0');
+            bin2 = bin2.TrimStart('0');
+
+
+            if (string.IsNullOrEmpty(bin1) || string.IsNullOrEmpty(bin2))
+            {
+                return "0";
+            }
+
+            string result = "0";
+
+            for (int i = bin2.Length - 1; i >= 0; i--)
+            {
+                if (bin2[i] == '1')
+                {
+                    string partialProduct = bin1;
+
+                    for (int j = 0; j < bin2.Length - 1 - i; j++)
+                    {
+                        partialProduct += '0';
+                    }
+
+                    result = AddBinary(result, partialProduct);
+
+                }
+            }
+
+            if (negativeResult && result != "0") result = TwosComplement(result);
+            return result;
+        }
+
+        private string DivideBinary(string bin1, string bin2)
+        {
+            if (string.IsNullOrEmpty(bin2.TrimStart('0')))
+            {
+                throw new DivideByZeroException();
+            }
+
+
+            bool negativeResult = (bin1[0] == '1') ^ (bin2[0] == '1');
+
+            if (bin1.StartsWith("1")) bin1 = TwosComplement(bin1);
+
+            if (bin2.StartsWith("1")) bin2 = TwosComplement(bin2);
+
+
+
+            bin1 = bin1.TrimStart('0');
+            bin2 = bin2.TrimStart('0');
+
+
+
+            if (string.IsNullOrEmpty(bin1)) return "0";
+
+            string result = "";
+            string currentDividend = "";
+
+
+            for (int i = 0; i < bin1.Length; i++)
+            {
+
+                currentDividend += bin1[i];
+                if (IsGreaterOrEqual(currentDividend, bin2))
+                {
+
+
+                    currentDividend = SubtractBinary(currentDividend, bin2);
+
+                    result += "1";
+                }
+                else
+                {
+                    result += "0";
+                }
+                currentDividend = currentDividend.TrimStart('0');
+            }
+
+            if (negativeResult && !string.IsNullOrEmpty(result.TrimStart('0'))) result = TwosComplement(result);
+
+            return result;
+        }
+
+        private int GetRequiredBitLength(int decimalValue, int baseNS)
+        {
+            int bitLength = 0;
+            int maxValue = 1;
+            while (maxValue < decimalValue)
+            {
+                maxValue *= 2;
+                bitLength++;
+            }
+
+            if (baseNS > 2 && bitLength > 0)
+            {
+                bitLength += 1;
+            }
+            else if (baseNS == 2 && decimalValue < 0)
+            {
+                bitLength += 1;
+            }
+
+            return Math.Max(bitLength, 4);
+        }
+
+        private bool IsGreaterOrEqual(string bin1, string bin2)
+        {
+            int n1 = bin1.Length;
+            int n2 = bin2.Length;
+
+            if (n1 > n2) return true;
+            if (n1 < n2) return false;
+
+            for (int i = 0; i < n1; i++)
+            {
+                if (bin1[i] > bin2[i]) return true;
+                if (bin1[i] < bin2[i]) return false;
+            }
+
+            return true;
         }
 
         private static bool CheckValidNum(string num, int inNS)
